a37e5e1a0cfd3a7fdf931a727eb329ff2ae69da4
meta <hosaka-corp@users.noreply.github.com>
2018-06-12 22:53:28 -0500

    take a block size as an argument; fix data positioning

--- py/make-data-gci.py
+++ py/make-data-gci.py
@@ -8,28 +8,36 @@ import binascii
 from fgx_format import *
 from fgx_encode import *
 
-BLOCKSIZE = 3
-NEW_SIZE = (BLOCKSIZE * 0x2000) + 0x40
 
-if len(sys.argv) < 4:
-    print("Usage: make-data-gci.py <input data filename> <GCI filename> <output .gci filename>")
+DATA_OFFSET = 0x240
+DENTRY_SIZE = 0x40
+BLOCK_SIZE = 0x2000
+DOL_HEADER_SIZE = 0x100
+
+if len(sys.argv) < 5:
+    print("Usage: make-data-gci.py <input data filename> <blocksize> <GCI filename> <output .gci filename>")
     exit(0)
 else:
     input_filename = sys.argv[1]
-    dentry_filename = sys.argv[2]
-    output_filename = sys.argv[3]
+    num_blocks = int(sys.argv[2])
+    dentry_filename = sys.argv[3]
+    output_filename = sys.argv[4]
 
-with open(input_filename) as f:
-    input_data = bytearray(f.read(), 'ascii')
+new_size = (num_blocks * BLOCK_SIZE) + DENTRY_SIZE
+
+with open(input_filename, "rb") as f:
+    data = f.read()
+    input_data = bytearray(data)
     input_len = len(input_data)
 
-if (input_len > (NEW_SIZE - 0x40 - 0x100)):
+if (input_len > (new_size - DENTRY_SIZE - DATA_OFFSET)):
     print("Your input file is too large to fit in {} blocks ({} bytes)".format(
-          BLOCKSIZE, input_len))
+          num_blocks, new_size))
+    print("File length is {} bytes".format(input_len))
     exit(-1)
 
-new_gci = gci(blocksize=BLOCKSIZE, gci_filename=dentry_filename)
-new_gci.raw_bytes[0x100:(0x100+input_len)] = input_data
+new_gci = gci(blocksize=num_blocks, gci_filename=dentry_filename)
+new_gci.raw_bytes[DATA_OFFSET:(DATA_OFFSET+input_len)] = input_data
 
 ofd = open(output_filename, "wb")
 ofd.write(new_gci.raw_bytes)
